Fixed error with bad quaternion interpretation

ahrs/mahony.py
@@ -1,6 +1,7 @@
 import numpy as np
 import ahrs
 from preprocess import preprocess
+import math
 
 mahony = ahrs.filters.Mahony()
 
@@ -12,4 +13,5 @@ while True:
     g = np.array((gx, gy, gz))
     mahony.updateIMU(q=Q, acc=a, gyr=g)
 
-    print(Q[0] * 180, Q[1], Q[2], Q[3], sep="\t", flush=True)
+    print(math.acos(Q[0]) * 2 * 57.2957795,
+          Q[1], Q[2], Q[3], sep="\t", flush=True)