234c0e4a8c1813a3b6288032fdf2620b409b2d60
Szymon Bednorz <dsonyyy@gmail.com>
2021-08-23 23:02:13 +0200

    discrete color fields v2

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 There is a list of my uploaded projects and exercises (from the latest to the oldest):
 
+- **discrete-color-fields** - a script generating pictures loosely inspired by [Jan Pamuła](https://pl.wikipedia.org/wiki/Jan_Pamu%C5%82a_(grafik))'s artworks:
+
+  <p align="center"><img width=200 src="discrete-color-fields/1629751570-9.png"><img width=200 src="discrete-color-fields/1629751415-9.png"><img width=200 src="discrete-color-fields/1629751485-9.png"><img width=200 src="discrete-color-fields/1629751297-9.png"></p>
+
 - **chaos-game** - implementation of [the chaos game](https://en.wikipedia.org/wiki/Chaos_game). [My Youtube video](https://www.youtube.com/watch?v=4fCrPIvEHP0).
 
 - **ahrs** - playing with attitude and heading reference systems using [ahrs library](https://pypi.org/project/AHRS/) and [MPU-6050](https://invensense.tdk.com/products/motion-tracking/6-axis/mpu-6050/) data.
@@ -12,7 +16,6 @@ There is a list of my uploaded projects and exercises (from the latest to the ol
   [cpp-stuff/angle-visualization](https://github.com/dsonyy/cpp-stuff) - visualizing quaternion rotations (...).  
   python-stuff/accelerometer-live-plot - below  
   
-
 - **accelerometer-live-plot** - real-time visualization tool for accelerometer data with 6 degrees of freedom. [Nice YouTube video](https://www.youtube.com/watch?v=J4pH3LHojVM)
 <p align="center"><img width=500 src="accelerometer-live-plot/example.png"></p>
 
--- discrete-color-fields/discrete-color-fields.py
+++ discrete-color-fields/discrete-color-fields.py
@@ -1,8 +1,9 @@
 import random
 import sys
+import time
 
-W = 1000
-H = 1000
+W = 800
+H = 800
 
 SVG_BEGIN = '<?xml version="1.0"?>\n \
 <svg xmlns="http://www.w3.org/2000/svg" \
@@ -43,11 +44,15 @@ def split_rects(rects):
         a = random.choice(("vertical", "horizontal"))
         try:
             if a == "vertical":
-                b = random.randrange(parent[0], parent[1])
+                # b = random.randrange(parent[0], parent[1])
+                b = parent[0] + (parent[1] - parent[0]) * \
+                    random.choice((.25, .5, .75))
                 rect0 = (parent[0], b, parent[2], parent[3])
                 rect1 = (b, parent[1], parent[2], parent[3])
             else:
-                b = random.randrange(parent[2], parent[3])
+                # b = random.randrange(parent[2], parent[3])
+                b = parent[2] + (parent[3] - parent[2]) * \
+                    random.choice((.25, .5, .75))
                 rect0 = (parent[0], parent[1], parent[2], b)
                 rect1 = (parent[0], parent[1], b, parent[3])
             splitted_rects.extend((rect0, rect1))
@@ -56,24 +61,24 @@ def split_rects(rects):
     return splitted_rects
 
 
-def gen_rects(lev):
-    rects = [(0, W, 0, H)]
-    for i in range(lev):
-        rects = split_rects(rects)
-    return rects
-
-
 def gen_svg(rects):
     svg = SVG_BEGIN
-    svg += SVG_BACKGROUND
+    # svg += SVG_BACKGROUND
 
     for rect in rects:
+        # col = get_random_color()
+        # col = get_gray_color()
+        # col = random.choice(("#420516", "#7D1935", "#B42B51", "#E63E6D", "#000"))
+        # col = random.choice(("#000", "#333", "#666", "#999", "#CCC", "#FFF"))
+        # col = random.choice(("#297F87", "#FFF7AE", "#F6D167", "#DF2E2E", "#000"))
+        col = random.choice(
+            ("#002366", "#0F52BA", "#FA8072", "#FFDAB9", "#000"))
         svg += '<rect ' + \
-            'x="' + str(rect[0]) + '" ' + \
-            'y="' + str(rect[2]) + '" ' + \
-            'width="' + str(rect[1] - rect[0]) + '" ' + \
-            'height="' + str(rect[3] - rect[2]) + '" ' + \
-            'style="fill:' + get_gray_color() + '" />\n'
+            'x="' + str(rect[0] - 1) + '" ' + \
+            'y="' + str(rect[2] - 1) + '" ' + \
+            'width="' + str(rect[1] - rect[0] + 2) + '" ' + \
+            'height="' + str(rect[3] - rect[2] + 2) + '" ' + \
+            'style="fill:' + col + '" />\n'
 
     svg += SVG_END
     return svg
@@ -85,16 +90,21 @@ def save(svg, name):
 
 
 def main():
+    t = int(time.time())
+
     lev = 10
     if len(sys.argv) > 1:
         lev = int(sys.argv[1])
 
-    rects = gen_rects(lev)
+    rects = [(0, W, 0, H)]
+    for i in range(lev):
+        rects = split_rects(rects)
+    svg = gen_svg(rects)
+    save(svg, "{}-{}.svg".format(t, i))
+
     print("X0", "X1", "Y0", "Y1", sep="\t")
     for rect in rects:
         print(*rect, sep="\t")
-    svg = gen_svg(rects)
-    save(svg, "output.svg")
 
 
 if __name__ == "__main__":
